2b92f77cac7592aad5de77943b80985344ef8e5c
saksham lakhera <41074890+Saksham-Lakhera@users.noreply.github.com>
2020-06-08 15:47:59 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorlayer.cost import cross_entropy_seq, cross_entropy_seq_with_mask
 from tqdm import tqdm
 from sklearn.utils import shuffle
-from data.twitter import data
+from data.chatapp import data
 from tensorlayer.models.seq2seq import Seq2seq
 from tensorlayer.models.seq2seq_with_attention import Seq2seqLuongAttention
 import os
